9456c17cecdc842db71f6c5698467b3f441700a9
hersson ceballos <herssonceballos@gmail.com>
2020-08-22 00:06:31 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class  Song
+  
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0 
+  @@genres = []
+  @@artists = []
+  
+ def initialize(name, artist, genre)
+  @@count += 1
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@artists << @artist
+  @@genres << @genre
+end 
+#     def artists=(artist_name)
+#   @artists = artists_name
+# end 
+  
+#   def artists
+#     @artists
+#   end
+  
+end 
