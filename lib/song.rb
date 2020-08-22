0dc1be06ea5c0f6635e2df9a93b9138652652945
hersson ceballos <herssonceballos@gmail.com>
2020-08-22 00:19:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,21 +35,11 @@ end
       else 
         genre_count[genre] = 1 
       end
-  end
+    end
     genre_count
-end
+  end
 
-# def self.genre_count
-#     genre_count = {}
-#     @@genres.each do |genre| 
-#       if genre_count[genre]
-#       genre_count[genre] += 1 
-#       else
-#       genre_count[genre] = 1
-#       end
-#     end
-#     genre_count
-#   end
+
   
 #     def artists=(artist_name)
 #   @artists = artists_name
